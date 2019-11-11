02936469a3a791691d8fdcf7416d76cd1a770fed
ezevin <ehzevin@gmail.com>
2019-11-11 14:54:21 -0500

    finished setting up basic persisting fetch requests for attractions

--- API/Routes/attractions.js
+++ API/Routes/attractions.js
@@ -5,9 +5,18 @@ const mongoose = require("mongoose");
 const Attraction = require('../models/attraction');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /attractions'
-  })
+  Attraction.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    })
 });
 
 router.post('/', (req, res, next) => {
@@ -35,28 +44,58 @@ router.post('/', (req, res, next) => {
 
 router.get('/:attractionId', (req, res, next) => {
   const id = req.params.attractionId;
-  if(id === 'special'){
-    res.status(200).json({
-      message: "You discovered the special ID",
-      id: id
+  Attraction.findById(id)
+    .exec()
+    .then(doc => {
+    console.log("From database", doc);
+    if (doc){
+       res.status(200).json(doc)
+    } else {
+      res.status(404).json({
+        message: "No valid entry found for provided ID"
+      })
+    }
     })
-  }else {
-    res.status(200).json({
-      message: 'You passed an ID'
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
     });
-  };
 });
 
 router.patch('/:attractionId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated Attraction!'
-  });
+  const id = req.params.attractionId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Attraction.update({ _id: id}, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 router.delete('/:attractionId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted Attraction!'
-  })
+  const id = req.params.attractionId;
+  Attraction.remove({_id: id})
+    .exec()
+    .then(result => {
+      console.log(docs);
+      res.status(200).json(docs)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    });
 })
 
 
