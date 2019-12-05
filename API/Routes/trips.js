aafeb5ecd3c29e0598579259ce675e0edf58d5a3
ezevin <ehzevin@gmail.com>
2019-12-05 16:03:46 -0500

    added trips controller

--- API/Routes/trips.js
+++ API/Routes/trips.js
@@ -5,126 +5,19 @@ const checkAuth = require('../middleware/check-auth');
 
 const Trip = require('../models/trip');
 const Attraction = require('../models/attraction');
+
+const TripsController = require('../controllers/trips');
+
 //Handle incoming GET requests to /trips //
-router.get('/', checkAuth, (req, res, next) => {
-  Trip.find()
-    .select('attraction quantity _id')
-    .populate('attraction', 'name location')
-    .exec()
-    .then(docs => {
-      res.status(200).json({
-        count: docs.length,
-        trips: docs.map(doc => {
-          return {
-            _id: doc._id,
-            attraction: doc.attraction,
-            quantity: doc.quantity,
-            request:  {
-              type: 'GET',
-              url: 'http://localhost:3002/trips/' + doc._id
-            }
-          };
-        })
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+router.get('/', checkAuth, TripsController.trips_get_all);
 
-router.post('/', checkAuth, (req, res, next) => {
-  Attraction.findById(req.body.attractionId)
-    .then(attraction => {
-      if(!attraction){
-        return res.status(404).json({
-          message: "Product not found."
-        });
-      }
-      const trip = new Trip({
-        _id: mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        attraction: req.body.attractionId
-      });
-      return trip.save();
-      })
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message:  "Trip stored",
-          createdTrip: {
-            _id: result._id,
-            attraction: result.attraction,
-            quantity: result.quantity
-          },
-          request: {
-            type: 'GET',
-            url: 'http://localhost:3002/trips/' + result._id
-          }
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
-});
+router.post('/', checkAuth, TripsController.trips_create_trips);
 
-router.get('/:tripId', checkAuth, (req, res, next) => {
-  Trip.findById(req.params.tripId)
-    .populate('attraction')
-    .exec()
-    .then(trip => {
-      if(!trip){
-        return res.status(404).json({
-          message: 'Trip not found'
-        })
-      }
-      res.status(200).json({
-        trip: trip,
-        request: {
-          type: 'GET',
-          url: 'http://localhost:3002/trips/'
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      })
-    });
-});
+router.get('/:tripId', checkAuth, TripsController.trips_get_trip);
 
-router.patch('/:tripId', checkAuth, (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated Trip!'
-  })
-})
+router.patch('/:tripId', checkAuth, TripsController.trips_update_trip);
 
-router.delete('/:tripId', checkAuth, (req, res, next) => {
-  Trip.remove({ _id: req.params.tripId })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: 'Trip deleted',
-        request: {
-          type: 'POST',
-          url: 'http://localhost:3002/trips/',
-          body: {
-            productId: 'ID',
-            quantity: 'Number'
-          }
-        }
-      })
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      })
-    });
-})
+router.delete('/:tripId', checkAuth, TripsController.trips_delete_trip);
 
 
 module.exports = router;
